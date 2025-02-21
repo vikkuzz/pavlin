701ab5fbefac111917b0136f9f8ca70d9b1fb16f
Vikkuzz <simpat007@yandex.ru>
2025-02-21 23:32:18 +0300

    fix: layout style

--- app/ads/new/page.js
+++ app/ads/new/page.js
@@ -4,7 +4,9 @@ import NewAdsContent from "@/components/newAdsContent";
 const NewAdsPage = () => {
   return (
     <div className="flex flex-col z-10 w-full items-center justify-between font-mono text-sm lg:flex">
-      <h1 className="text-4xl">Новоград Павлино | Написать объявление</h1>
+      <h1 className="text-3xl md:text-4xl p-3">
+        Новоград Павлино | Написать объявление
+      </h1>
       <div className="flex flex-col gap-4 justify-center items-center mt-8">
         <div className="flex flex-wrap gap-3 md:min-w-[600px]">
           <NewAdsContent />
