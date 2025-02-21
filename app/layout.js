4b654791fc093f2ed8241dd6e4a2eaf17afbbe24
Vikkuzz <simpat007@yandex.ru>
2025-02-21 23:23:16 +0300

    fix: ym init

--- app/layout.js
+++ app/layout.js
@@ -26,9 +26,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Head>
-          <Script id="metrika-counter" defer strategy="afterInteractive">
-            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        <Script id="metrika-counter" defer strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();
    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -40,8 +39,7 @@ export default function RootLayout({ children }) {
         accurateTrackBounce:true,
         webvisor:true
    });`}
-          </Script>
-        </Head>
+        </Script>
         <AuthProvider>
           <AntdRegistry>
             <LayoutPage>{children}</LayoutPage>
