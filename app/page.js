ac6664525bcd8ea166f858b51ec053257a0867a3
Vikkuzz <simpat007@yandex.ru>
2025-02-24 23:40:47 +0300

    feat: add video

--- app/page.js
+++ app/page.js
@@ -7,12 +7,18 @@ export default async function MyApp() {
       <div className="flex flex-col gap-4 justify-center items-center mt-8">
         <div className="flex flex-col gap-4 p-3">
           <h2>Новости</h2>
-          <h3>
-            20.02.2025 "Самолет" выложил новое видео о ходе строительства
-            новостроек
-          </h3>
-        </div>
-        <VideoPlayer url="https://media.samolet.ru/video/%D0%9D%D0%BE%D0%B2%D0%BE%D0%B3%D1%80%D0%B0%D0%B4%20%D0%9F%D0%B0%D0%B2%D0%BB%D0%B8%D0%BD%D0%BE%20%D1%84%D0%B5%D0%B2%D1%80%D0%B0%D0%BB%D1%8C%202025.mp4" />
+          <div className="flex flex-col gap-4">
+            <h3>
+              20.02.2025 "Самолет" выложил новое видео о ходе строительства
+              новостроек
+            </h3>
+            <VideoPlayer url="https://media.samolet.ru/video/%D0%9D%D0%BE%D0%B2%D0%BE%D0%B3%D1%80%D0%B0%D0%B4%20%D0%9F%D0%B0%D0%B2%D0%BB%D0%B8%D0%BD%D0%BE%20%D1%84%D0%B5%D0%B2%D1%80%D0%B0%D0%BB%D1%8C%202025.mp4" />
+          </div>
+          <div className="flex flex-col gap-4">
+            <h3>23.02.2025 Видео одного из будущих жильцов. Корпус 6.</h3>
+            <VideoPlayer url="/video/IMG_4270.MP4" />
+          </div>
+        </div>{" "}
       </div>
     </div>
   );
