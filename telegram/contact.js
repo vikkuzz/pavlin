24fdea29449aca40fb364d7048d9b092f808cbc3
Vikkuzz <simpat007@yandex.ru>
2025-02-24 17:35:14 +0300

    fix: text bot

--- telegram/contact.js
+++ telegram/contact.js
@@ -1,10 +1,9 @@
 export default async function sendMessageToTelegram(userInput) {
   const { name, email, message } = userInput;
   const text = `
-    Отправлено с https://www.ngpavlino.ru 
-    Новое сообщение от ${name} 
-    Email: ${email} 
     Сообщение: ${message}
+    Email: ${email} 
+    Отправлено с https://www.ngpavlino.ru 
   `;
   const telegramUrl = `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID}&text=${text}`;
   try {
